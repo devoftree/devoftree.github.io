54f6382018b9ac6393e5f2a1e725408e05cc6f30
DevOfTree <82230534+devoftree@users.noreply.github.com>
2021-04-23 17:55:27 -0300

    Automated testnet deployment.

---
+++ static/js/10.e7fba62a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@treedefi/interface"]=this["webpackJsonp@treedefi/interface"]||[]).push([[10],{1193:function(e,i){}}]);
+//# sourceMappingURL=10.e7fba62a.chunk.js.map
---
+++ static/js/9.3fb92cb6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@treedefi/interface"]=this["webpackJsonp@treedefi/interface"]||[]).push([[9],{1067:function(e,i){}}]);
+//# sourceMappingURL=9.3fb92cb6.chunk.js.map
